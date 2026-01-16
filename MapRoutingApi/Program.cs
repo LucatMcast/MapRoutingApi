25eb80ad85abe0ac541c90a3d8783f2d8fde3edf
LucatMcast <156658528+LucatMcast@users.noreply.github.com>
2026-01-16 20:38:24 +0100

    Add files via upload

---
+++ MapRoutingApi/Program.cs
@@ -0,0 +1,32 @@
+using MapRoutingApi.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register MapService as Singleton
+builder.Services.AddSingleton<IMapService, MapService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
